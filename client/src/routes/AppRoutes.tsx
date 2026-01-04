229484d33e8070e5837cd266f5e1fa7bbf5def15
Gorbatuq <gorbataqvos@gmail.com>
2026-01-04 13:34:29 +0100

    Create Welcome page, and upgrade route logic

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,43 +2,68 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthPage } from "../pages/AuthPage";
 import { TodoPage } from "../pages/TodoPage";
 import { ProfilePage } from "../pages/ProfilePage";
-import { ProtectedRoute } from "./ProtectedRoute";
 import { ResetPasswordPage } from "../pages/ResetPasswordPage";
+import { WelcomePage } from "../pages/WelcomePage";
 import { useMe } from "../hooks/auth/useMe";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { PublicOnlyRoute } from "./PublicOnlyRoute";
 
 export const AppRoutes = () => {
   const { data: user, isLoading } = useMe();
-
   if (isLoading) return <p className="text-center mt-10">Checking login...</p>;
+
+  const isAuth = !!user;
+  const entry = isAuth ? "/todo" : "/welcome";
+
   return (
     <Routes>
+      <Route path="/" element={<Navigate to={entry} replace />} />
+
       <Route
-        path="/"
-        element={!user ? <AuthPage /> : <Navigate to="/todo" replace />}
+        path="/welcome"
+        element={
+          <PublicOnlyRoute isAuth={isAuth}>
+            <WelcomePage />
+          </PublicOnlyRoute>
+        }
       />
+
+      <Route
+        path="/auth"
+        element={
+          <PublicOnlyRoute isAuth={isAuth}>
+            <AuthPage />
+          </PublicOnlyRoute>
+        }
+      />
+
       <Route
         path="/reset-password"
         element={
-          !user ? <ResetPasswordPage /> : <Navigate to="/todo" replace />
+          <PublicOnlyRoute isAuth={isAuth}>
+            <ResetPasswordPage />
+          </PublicOnlyRoute>
         }
       />
-      {/*  */}
+
       <Route
         path="/todo"
         element={
-          <ProtectedRoute isAuth={!!user}>
+          <ProtectedRoute isAuth={isAuth}>
             <TodoPage />
           </ProtectedRoute>
         }
       />
+
       <Route
         path="/profile"
         element={
-          <ProtectedRoute isAuth={!!user}>
+          <ProtectedRoute isAuth={isAuth}>
             <ProfilePage />
           </ProtectedRoute>
         }
       />
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
